2a2ed976fe057be705ac8b61ad06a1a1f8ed371b
marcos <marcos@psicodelicoarts@hotmail.com>
2017-04-23 11:33:16 -0300

    component App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,44 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import Profile from './github/Profile.jsx'
 
 class App extends Component {
     constructor(props){
         super(props)
         this.state = {
-            username: 'marcos',
+            username: 'brugnaro',
             userData: [],
             userRepos: [],
             perPage: 5
         }
     }
 
+    //get user data
+    getUserDate(){
+        $.ajax({
+            url: 'https://api.github.com/users/'+this.state.username+'?client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret,
+            dataType: 'json',
+            cache: false,
+            success: function(data){
+                this.setState({userData: data});
+                console.log(data);
+            }.bind(this),
+            error: function(xhr, status, err){
+                this.setState({username: null});
+                alert(err);
+            }.bind(this)
+
+        });
+    }
+
+    componentDidMount(){
+        this.getUserDate();
+    }
+
     render(){
         return(
             <div>
-                {this.props.clientId}    
+                <Profile userData = {this.state.userData} />
             </div>
         )
     }
